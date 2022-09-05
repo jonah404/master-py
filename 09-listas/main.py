0cfe8c2cbd1c0f3cb5b4df52e9059568076df34b
jonah404 <jonahlalvarez@gmail.com>
2022-09-05 20:19:52 -0300

    masterpy

---
+++ 09-listas/main.py
@@ -0,0 +1,20 @@
+"""
+LISTAS (arrays)
+son colecciones o conjuntos de datos/valores, bajo un unico nombre.
+Para acceder a esos valores podemos usar un indice numerico.
+"""
+
+# Definir lista
+peliculas = ["batman", "hulk", "superman"]
+
+cantantes = list(('korn', 'BTMH', 'LP'))
+
+years = list(range(2020, 2050))
+
+variada = ["jona", 4, True, 4.5]
+
+
+print(peliculas)
+print(cantantes)
+print(years)
+print(variada)
