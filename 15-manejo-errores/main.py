32fe315f3ea32969ad8e9c4209343a6129acf637
jonah404 <jonahlalvarez@gmail.com>
2022-09-20 19:29:34 -0300

    masterpy

--- 15-manejo-errores/main.py
+++ 15-manejo-errores/main.py
@@ -1,6 +1,6 @@
 # Capturar excepciones y manejar errores en código
 # susceptible a fallos/errores
-
+"""
 try:
 
     nombre = input("¿Cuál es tu nombre?: ")
@@ -16,3 +16,37 @@ else:
 
 finally:
     print("Fin de la iteración")
+"""
+
+# Multiples excepciones
+"""
+try:
+    numero = int(input("Numero para elevarlo al cuadrado: "))
+    print("El cuadrado es: " + str(numero*numero))
+except TypeError:
+    print("Debes convertir tus cadenas a enteros.")
+except ValueError:
+    print("Introduce un numero correcto.")
+except Exception as e:
+    print("Ha ocurrido un error: ", type(e).__name__) #Detecta el tipo de error
+"""
+
+#Exepciones personalizadas o lanzar excepcion (rasie)
+try:
+    nombre = input("Introduce tu nombre: ")
+    edad = int(input("Ingresa tu edad: "))
+
+    if edad < 5 or edad > 110:
+        raise ValueError("La edad introducida no es correcta")
+    elif len(nombre) <= 1:
+        raise ValueError("El nombre no está completo")
+    else:
+        print(f"Bienvenido! {nombre}")
+except ValueError:
+    print("Introducce los datos correctamente")
+except Exception as e:
+    print("Existe un error: ", e)
+
+
+
+
