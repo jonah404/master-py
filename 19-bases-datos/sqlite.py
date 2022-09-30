5ab6c5a6d0c0cc4f7b1bb853fc52f640b0381e0e
jonah404 <jonahlalvarez@gmail.com>
2022-09-30 19:50:08 -0300

    masterpy

---
+++ 19-bases-datos/sqlite.py
@@ -0,0 +1,45 @@
+# sqlite viene integrado con python.
+# Importar módulo
+import sqlite3
+
+# Conexión a sqlite
+conexion = sqlite3.connect("pruebas.db") #Crea el archivo
+
+# Crear cursor (el cursor es el que permite ejecutar las consultas SQL)
+cursor = conexion.cursor()
+
+# Crear tabla
+cursor.execute("CREATE TABLE IF NOT EXISTS productos("+
+"id INTEGER PRIMARY KEY AUTOINCREMENT, "+
+"titulo VARCHAR(255), "+
+"descripcion TEXT, "+
+"precio INT(255)"+
+")")
+
+# Guardar cambios
+conexion.commit()
+
+# Insertar datos
+cursor.execute("INSERT INTO productos VALUES (null, 'Segundo producto', 'Descripcion del producto', 23)")
+conexion.commit()
+
+# Lectura de datos de una tabla (listar datos)
+cursor.execute("SELECT * FROM productos")
+print(cursor)
+
+productos = cursor.fetchall() #Trae todos los datos creados
+
+for producto in productos:
+    print("Título: ", producto[1])
+    print("Descripción: ", producto[2])
+    print("Precio: ", producto[3])
+    print("\n")
+
+cursor.execute("SELECT titulo FROM productos")
+producto = cursor.fetchone() #Saca el primer producto creado
+print(producto)
+
+# Cerrar conexión (sino el fichero queda abierto y los cambios no se guardan)
+conexion.close()
+
+
