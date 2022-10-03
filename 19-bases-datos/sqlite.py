e6942942b0478db184d1a9c1aab37c090db0146d
jonah404 <jonahlalvarez@gmail.com>
2022-10-03 19:54:26 -0300

    masterpy

--- 19-bases-datos/sqlite.py
+++ 19-bases-datos/sqlite.py
@@ -9,27 +9,50 @@ conexion = sqlite3.connect("pruebas.db") #Crea el archivo
 cursor = conexion.cursor()
 
 # Crear tabla
-cursor.execute("CREATE TABLE IF NOT EXISTS productos("+
-"id INTEGER PRIMARY KEY AUTOINCREMENT, "+
-"titulo VARCHAR(255), "+
-"descripcion TEXT, "+
-"precio INT(255)"+
-")")
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS productos(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo VARCHAR(255),
+    descripcion TEXT,
+    precio INT(255)
+);
+""")
+
 
 # Guardar cambios
 conexion.commit()
 
 # Insertar datos
+"""
 cursor.execute("INSERT INTO productos VALUES (null, 'Segundo producto', 'Descripcion del producto', 23)")
 conexion.commit()
+"""
+
+# Borrar registros
+#cursor.execute("DELETE FROM productos")
+#conexion.commit()
+
+# Insertar muchos registros de una
+productos = [ 
+    ("Ordenador", "Buen pc", 700),
+    ("Telefono", "Buen telefono", 450),
+    ("Placa", "Buena placa", 500),
+    ("Tablet", "Buena tablet", 600),
+]
+cursor.executemany("INSERT INTO productos VALUES (null, ?,?,?)", productos)# executemany ejecuta varias consultas, con el signo "?" asigna los valores
+conexion.commit()
+
+# Update, actualizar datos
+cursor.execute("UPDATE productos SET precio = 555 WHERE precio = 500")
 
 # Lectura de datos de una tabla (listar datos)
-cursor.execute("SELECT * FROM productos")
+cursor.execute("SELECT * FROM productos WHERE precio >= 500")
 print(cursor)
 
 productos = cursor.fetchall() #Trae todos los datos creados
 
 for producto in productos:
+    print("id: ", producto[0])
     print("Título: ", producto[1])
     print("Descripción: ", producto[2])
     print("Precio: ", producto[3])
