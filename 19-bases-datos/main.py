12eee9df38fe4969b932cb26bca23c07486ca8ae
jonah404 <jonahlalvarez@gmail.com>
2022-10-06 20:17:18 -0300

    masterpy

--- 19-bases-datos/main.py
+++ 19-bases-datos/main.py
@@ -34,8 +34,33 @@ precio FLOAT(10,2) NOT NULL,
 CONSTRAINT pk_vehiculo PRIMARY KEY(id)
 )
 """)
-
+"""
 cursor.execute("SHOW TABLES")
 
 for table in cursor:
     print(table)
+"""
+#cursor.execute("INSERT INTO vehiculos VALUES(null, 'Opel', 'Astra', 18123)")
+coches = [
+    ('Seat', 'Ibiza', 15456),
+    ('Ford', 'Focus', 14556),
+    ('Honda', 'Civic', 14563),
+    ('Jeep', 'Willys', 4564)
+]
+
+# cursor.executemany("INSERT INTO vehiculos VALUES (null, %s, %s, %s)", coches) # %s es una variable que permite sustituir por otros datos 
+
+database.commit()
+
+cursor.execute("SELECT * FROM vehiculos WHERE precio <= 15000 AND marca = 'Jeep'")
+
+result = cursor.fetchall() # fetchall() (saca todos los datos)
+
+print("Todos los vehículos:")
+for coche in result:
+    print(coche[1], coche[3])
+
+cursor.execute("SELECT * FROM vehiculos")
+coche = cursor.fetchone() # fetchone() (saca solo un valor)
+print(coche)
+
