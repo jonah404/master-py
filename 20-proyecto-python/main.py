e04d501ad6820fcdc5cc374178cad89bc130b2e3
jonah404 <jonahlalvarez@gmail.com>
2022-10-11 20:13:48 -0300

    proyecto_masterpy

--- 20-proyecto-python/main.py
+++ 20-proyecto-python/main.py
@@ -7,4 +7,25 @@ Proyecto Python mysql:
 - Crear nota, mostrar nota, borrarlas (abm)
 """
 
+print("""
+Acciones disponibles:
+    - Registro
+    - Login
+""")
+
+accion = input("¿Que quieres hacer?: ")
+
+if accion == "Registro":
+    print("\nOk, vamos a registrarte en el sistema...")
+
+    nombre   = input("¿Cuál es tu nombre? ")
+    apellido = input("¿Cuál es tu apellido? ")
+    mail     = input("Ingresa tu email: ")
+    passwd   = input("Ingresa una contraseña para acceder: ")
+
+elif accion == "Login":
+    print("Por favor identifícate en el sistema...")
+
+    mail   = input("Ingresa tu email: ")
+    passwd = input("Ingresa la contraseña: ")
 
