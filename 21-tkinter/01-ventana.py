a3445ae98345ab32fb63d1a75ef0209262bbd187
jonah404 <jonahlalvarez@gmail.com>
2022-10-24 19:31:16 -0300

    masterpy

--- 21-tkinter/01-ventana.py
+++ 21-tkinter/01-ventana.py
@@ -2,32 +2,58 @@
 from tkinter import *
 import os.path # Para comprobar la existencia de archivos
 
-# Crear la ventana raiz
-ventana = Tk()
 
-# Titulo de la ventana
-ventana.title("Titulo de la ventana")
+class Programa:
+    def __init__(self):
+        self.title = "Master en Python"
+        self.icon = './imagenes/terminal.ico'
+        self.icon_alt = './21-tkinter/imagenes/terminal.ico'
+        self.size = "770x470"
+        self.rezisable = False
 
-# Comprobar si existe un archivos
-ruta_icono = os.path.abspath('./imagenes/terminal.ico')
+    def cargar(self):
+        # Crear la ventana raiz
+        ventana = Tk()
+        self.ventana = ventana
+        # Titulo de la ventana
+        ventana.title(self.title)
 
-if not os.path.isfile(ruta_icono):
-    ruta_icono = os.path.abspath('./21-tkinter/imagenes/terminal.ico')
+        # Comprobar si existe un archivos
+        ruta_icono = os.path.abspath(self.icon)
 
-# Icono de la ventana
-ventana.iconbitmap(ruta_icono)
+        if not os.path.isfile(ruta_icono):
+            ruta_icono = os.path.abspath(self.icon_alt)
 
-# Mostrar texto en el programa
-texto = Label(ventana, text=ruta_icono)
-texto.pack()
+        # Icono de la ventana
+        ventana.iconbitmap(ruta_icono)
 
-
-# Cambiar el tamaño de la ventana
-ventana.geometry("750x450")
-
-# Bloquear el tamaño de la ventana
-ventana.resizable(0, 0) #los parámetros significan valores horizontal y vertical 0 y 1 (false y true)
+        # Mostrar texto en el programa
+        texto = Label(ventana, text=ruta_icono)
+        texto.pack()
 
 
-# Arrancar y mostrar ventana hasta que se cierre
-ventana.mainloop()
+        # Cambiar el tamaño de la ventana
+        ventana.geometry(self.size)
+
+        # Bloquear el tamaño de la ventana
+        if self.rezisable:
+            ventana.resizable(1, 1) #los parámetros (0, 0) significan valores horizontal y vertical 0 y 1 (false y true)
+        else:
+            ventana.resizable(0, 0)
+
+
+    def addTexto(self, dato):
+        texto = Label(self.ventana, text=dato)
+        texto.pack()
+
+    def mostrar(self):
+        # Arrancar y mostrar la ventana hasta que se cierre
+        self.ventana.mainloop()
+
+#Instanciar el programa
+programa = Programa()
+programa.cargar()
+programa.addTexto("Hola!")
+programa.addTexto("Soy un texto.")
+programa.mostrar()
+
