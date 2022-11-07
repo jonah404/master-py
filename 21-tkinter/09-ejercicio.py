34cea808676d9d9c684bf6d6a28ba78460c1c76b
jonah404 <jonahlalvarez@gmail.com>
2022-11-07 19:09:35 -0300

    master py

--- 21-tkinter/09-ejercicio.py
+++ 21-tkinter/09-ejercicio.py
@@ -11,23 +11,57 @@ from tkinter import messagebox
 ventana = Tk()
 ventana.title("Ejercicio completo con tkinter")
 ventana.config(bd=25)
+ventana.geometry("400x400")
+
+def sumar():
+    resultado.set(float(numero1.get()) + float(numero2.get()))
+    mostrarResultado()
+
+def restar():
+    resultado.set(float(numero1.get()) - float(numero2.get()))
+    mostrarResultado()
+
+def multiplicar():
+    resultado.set(float(numero1.get()) * float(numero2.get()))
+    mostrarResultado()
+
+def dividir():
+    resultado.set(float(numero1.get()) / float(numero2.get()))
+    mostrarResultado()
+
+def mostrarResultado():
+    messagebox.showinfo("Resultado", f"El resultado de la operacion es: {resultado.get()}")
+    numero1.set("")
+    numero2.set("")
 
 numero1=StringVar()
 numero2=StringVar()
 resultado=StringVar()
 
-Label(ventana, text="Primer número: ").pack()
-Entry(ventana, textvariable=numero1).pack()
+marco = Frame(ventana, width=300, height=200)
+marco.config(
+    padx=15,
+    pady=15,
+    bd=5,
+    relief=SOLID
+)
+marco.pack(side=TOP, anchor=CENTER)
+marco.pack_propagate(False)
 
-Label(ventana, text="Segundo número: ").pack()
-Entry(ventana, textvariable=numero2).pack()
 
-Label(ventana, text="").pack()
 
-Button(ventana, text="Sumar").pack(side="left")
-Button(ventana, text="Restar").pack(side="left")
-Button(ventana, text="Multiplicar").pack(side="left")
-Button(ventana, text="Dividir").pack(side="left")
+Label(marco, text="Primer número: ").pack()
+Entry(marco, textvariable=numero1, justify=CENTER).pack()
+
+Label(marco, text="Segundo número: ").pack()
+Entry(marco, textvariable=numero2, justify=CENTER).pack()
+
+Label(marco, text="").pack()
+
+Button(marco, text="Sumar", command=sumar).pack(side="left", fill=X, expand=YES)
+Button(marco, text="Restar", command=restar).pack(side="left", fill=X, expand=YES)
+Button(marco, text="Multiplicar", command=multiplicar).pack(side="left", fill=X, expand=YES)
+Button(marco, text="Dividir", command=dividir).pack(side="left", fill=X, expand=YES)
 
 
 
