5b7b00e285ea044c1c7636b91ed858396afd0305
jonah404 <jonahlalvarez@gmail.com>
2022-11-10 20:50:22 -0300

    master py

--- 21-tkinter/11-formularios2.py
+++ 21-tkinter/11-formularios2.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 ventana = Tk()
-ventana.geometry("800x300")
+ventana.geometry("800x500")
 
 encabezado = Label(ventana, text="Formularios 2")
 encabezado.config(
@@ -58,9 +58,48 @@ Checkbutton(
 mostrar = Label(ventana)
 mostrar.grid(row=4, column=0)
 
-
 # Radio Buttons
+def marcar():
+    marcado.config(text=opcion.get())
+
+opcion = StringVar()
+opcion.set(None)
+
+Label(ventana, text="¿Cuál es tu género?").grid(row=5)
+
+Radiobutton(
+    ventana, 
+    text="Masculino",
+    value="Masculino",
+    variable=opcion,
+    command=marcar
+).grid(row=6)
+Radiobutton(
+    ventana,
+    text="Femenino",
+    value="Femenino",
+    variable=opcion,
+    command=marcar
+).grid(row=7)
+
+marcado = Label(ventana)
+marcado.grid(row=8)
 
 # Option Menu
+def seleccionar():
+    seleccionado.config(text=opcion.get())
+
+opcion = StringVar()
+opcion.set("Opcion 1")
+
+Label(ventana, text="Selecciona una opción").grid(row=5, column=1)
+
+select = OptionMenu(ventana, opcion, "Opción 1", "Opción 2", "Opción 3")
+select.grid(row=6, column=1)
+
+Button(ventana, text="Ver", command=seleccionar).grid(row=7, column=1)
+
+seleccionado = Label(ventana)
+seleccionado.grid(row=8, column=1)
 
 ventana.mainloop()
