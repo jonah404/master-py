52ba3d4c7bf9d3c1441b184a2bce870eef9c3fed
jonah404 <jonahlalvarez@gmail.com>
2022-11-12 18:07:18 -0300

    master py

---
+++ 21-tkinter/13-proyecto.py
@@ -0,0 +1,12 @@
+"""
+Crear un programa que tenga:
+- Ventana
+- Tamaño fijo
+- No redimensionable
+- Un Menu
+- Diferentes pantallas
+- Formulario de añadir productos
+- Guardar datos temporalmente
+- Mostrar datos listados en la pantalla principal
+- Opcion de salir
+"""
