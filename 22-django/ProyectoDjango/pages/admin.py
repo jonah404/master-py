ae0e61aa021b79c8d4c5cff3081c83d821798020
jonah404 <jonahlalvarez@gmail.com>
2023-01-26 19:36:05 -0300

    master py

--- 22-django/ProyectoDjango/pages/admin.py
+++ 22-django/ProyectoDjango/pages/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Page
 
 # Register your models here.
+
+admin.site.register(Page)
+
+#Configuración del panel
+title = "Panel de gestión"
+subtitle = "Proyecto Django"
+
+admin.site.site_header = title
+admin.site.site_title = title
+admin.site.index_title = subtitle
+
--- 22-django/ProyectoDjango/pages/models.py
+++ 22-django/ProyectoDjango/pages/models.py
@@ -15,3 +15,4 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
