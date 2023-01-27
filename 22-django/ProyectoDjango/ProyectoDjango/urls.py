3e610da9c62ac0ba6f92b67d81cb5c67c77721a5
jonah404 <jonahlalvarez@gmail.com>
2023-01-27 20:24:33 -0300

    master py

--- 22-django/ProyectoDjango/ProyectoDjango/urls.py
+++ 22-django/ProyectoDjango/ProyectoDjango/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainapp import views
+import pages.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('inicio/', views.index, name="inicio"),
-    path('sobre-nosotros/', views.about, name="about")
+    path('pagina/<str:slug>', pages.views.page, name="page")
 ]
--- 22-django/ProyectoDjango/pages/views.py
+++ 22-django/ProyectoDjango/pages/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Page
 
 # Create your views here.
+def page(request, slug):
+
+    page = Page.objects.get(slug=slug)
+
+    return render(request, "pages/page.html", {
+        "page": page
+    })
