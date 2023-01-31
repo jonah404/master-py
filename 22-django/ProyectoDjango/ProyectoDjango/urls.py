d1767d4373566319f6eb5657e080cd4491fadd9c
jonah404 <jonahlalvarez@gmail.com>
2023-01-31 19:43:48 -0300

    master py

--- 22-django/ProyectoDjango/ProyectoDjango/urls.py
+++ 22-django/ProyectoDjango/ProyectoDjango/urls.py
@@ -14,13 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from mainapp import views
-import pages.views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index"),
-    path('inicio/', views.index, name="inicio"),
-    path('pagina/<str:slug>', pages.views.page, name="page")
+    path('', include('mainapp.urls')),
+    path('', include('pages.urls'))
 ]
