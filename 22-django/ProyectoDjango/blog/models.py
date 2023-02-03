bad7e7a379bd27efdc41cffe68e4e283d5804de5
jonah404 <jonahlalvarez@gmail.com>
2023-02-02 22:51:32 -0300

    master py

---
+++ 22-django/ProyectoDjango/blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Nombre')
+    description = models.CharField(max_length=255, verbose_name='Descripcion')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creado el')
+
+    class Meta:
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Categorías'
+
+    def __str__(self):
+        return self.name
