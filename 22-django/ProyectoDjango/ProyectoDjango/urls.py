dcf8a99f4c0417adb5267970d9b385fad9c57a41
jonah404 <jonahlalvarez@gmail.com>
2023-02-07 20:31:11 -0300

    master py

--- 22-django/ProyectoDjango/ProyectoDjango/urls.py
+++ 22-django/ProyectoDjango/ProyectoDjango/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls')),
-    path('', include('pages.urls'))
+    path('', include('pages.urls')),
+    path('', include('blog.urls'))
 ]
