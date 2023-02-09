fa95995fe8dded697679c7a1ae1eedd8c6238fe5
jonah404 <jonahlalvarez@gmail.com>
2023-02-09 20:34:09 -0300

    master py

---
+++ 22-django/ProyectoDjango/blog/processor.py
@@ -0,0 +1,9 @@
+from blog.models import Category
+
+def get_categories(request):
+    
+    categories = Category.objects.values_list('id', 'name')
+    
+    return {
+        'categories': categories
+    }
